1b3d16b50cda21c4eaa2c1b7328dbe2ab3f2fe51
ShitalBhise <bhiseshital27059@gmail.com>
2022-03-05 22:19:03 +0530

    Welcome to BST

---
+++ src/bst/BinaryMain.java
@@ -0,0 +1,10 @@
+package bst;
+
+public class BinaryMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Binary Serch String");
+	}
+
+}
---
+++ src/bst/BinaryNode.java
@@ -0,0 +1,12 @@
+package bst;
+
+public class BinaryNode {
+	/*
+	 * 1. Declaring the variables that are used as left node,right node,root node
+	 * and data;
+	 */
+	int data;
+	BinaryNode left;
+	BinaryNode right;
+
+}
